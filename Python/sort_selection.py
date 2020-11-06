2b0cff1b343cc21f46d0b4e720a8dc1f012c64b2
sayand0122 <sayandutta.jan@gmail.com>
2020-11-06 17:58:31 +0530

    selection sort in python

---
+++ Python/sort_selection.py
@@ -0,0 +1,20 @@
+def selection_sort(a):
+  for i in range(len(a)):
+    min_index = i
+    for j in range(i+1,len(a)):
+      if a[min_index]>a[j]:
+        min_index=j
+    tmp=a[i]
+    a[i]=a[min_index]
+    a[min_index]=tmp
+  return a
+  
+a=list(map(int, input().split()))
+a1=a.sort()
+sort_a=selection_sort(a)
+print("Sorted array = ",a)
+
+if a == a1:
+  print("Both inbuilt and my logic have same input")
+else:
+  print("Answers are not the same")
