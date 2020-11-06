4788d35bb62083f52eaaf736725b16928be1c06c
sayand0122 <sayandutta.jan@gmail.com>
2020-11-06 17:57:46 +0530

    insertion sort in python

---
+++ Python/sort_insertion.py
@@ -0,0 +1,19 @@
+def insertion_sort(a):
+  for i in range(1,len(a)):
+    k=a[i]
+    j=i-1
+    while j>=0 and k<=a[j]:
+      a[j+1]=a[j]
+      j-=1
+    a[j+1]=k
+  return a
+  
+a=list(map(int, input().split()))
+a1=a.sort()
+sort_a=insertion_sort(a)
+print("Sorted array = ",a)
+
+if a == a1:
+  print("Both inbuilt and my logic have same input")
+else:
+  print("Answers are not the same")
