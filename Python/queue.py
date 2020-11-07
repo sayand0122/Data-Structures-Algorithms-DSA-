cc10847b0218fa1ce2a17ffbb249d9a3c2315de4
SaatvikRamani <saatvikramani777@gmail.com>
2020-11-07 11:37:54 +0530

    Create queue.py

---
+++ Python/queue.py
@@ -0,0 +1,35 @@
+queue = []
+
+
+def enqueue(item):
+    queue.append(item)
+
+
+def deque():
+    if queue:
+        print(queue.pop(0))
+    else:
+        print("Queue is empty")
+
+
+def display():
+    if queue:
+        print(*queue)
+    else:
+        print("Queue is empty")
+
+
+print("\n1. Insert an element\n2. Delete an element\n3. Display the elements\n4. Exit\n")
+
+while True:
+    op = int(input("Enter your choice : "))
+    if op == 1:
+        enqueue(input("Enter your input : "))
+    elif op == 2:
+        deque()
+    elif op == 3:
+        display()
+    elif op == 4:
+        quit()
+    else:
+        print("Wrong choice !!! Try again\n")
